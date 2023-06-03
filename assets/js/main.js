d4a993d6df085faeccd60f8e41bf68f147cd3d1b
JWord <jwren4170@att.net>
2023-06-02 19:10:44 -0500

    Added active state to button

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ import { calculateAge } from './age-calculations.js';
 const button = document.querySelector('.svg');
 
 button.addEventListener('click', (e) => {
+	button.style.backgroundColor = '#000';
 	e.preventDefault();
 	validateFormInputs();
 	calculateAge();
